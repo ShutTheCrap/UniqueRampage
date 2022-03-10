9a8421777d7bae992090a527356da756a836de9e
ShutTheCrap <101266283+ShutTheCrap@users.noreply.github.com>
2022-03-10 18:32:05 +0700

    Create robloxaddresses.lua

---
+++ robloxaddresses.lua
@@ -0,0 +1,12 @@
+-- rebased to 0x0
+
+r_print: 0x34A410
+r_spawn: __cdecl 0x3C0AB0
+r_luau_deserialize: ??? 0x12FCB70
+getdatamodel1: __thiscall 0xD89130
+getdatamodel2: 0xD89EB0
+get_lua_state: __thiscall 0x3A8EB0
+r_lua_pushvfstring: 0x12F8A70
+r_gettaskscheduler: 0x11F1AF0
+r_index2adr: __fastcall 0x12F8C30
+r_sandboxthread: __fastcall 0x3E45A0
